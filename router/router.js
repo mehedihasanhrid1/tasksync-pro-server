e24f7f124d817fbc9ba2be48958542009207515c
Mehedi Hasan <mehedihasanhrid1@gmail.com>
2023-12-22 18:31:23 +0600

    Edit a task implimented

--- router/router.js
+++ router/router.js
@@ -4,5 +4,8 @@ const router = express.Router();
 const taskController = require('../task/taskcontroller');
 
 router.post('/tasks', taskController.createTask);
+router.get('/tasks', taskController.getTasks);
+router.delete('/tasks/:id', taskController.deleteTask);
+router.put('/tasks/:id', taskController.updateTask);
 
 module.exports = router;
--- task/taskcontroller.js
+++ task/taskcontroller.js
@@ -12,6 +12,59 @@ async function createTask(req, res) {
   }
 }
 
+
+async function getTasks(req, res) {
+  try {
+    const { userEmail, type } = req.query;
+    const tasks = await Task.find({ email: userEmail, type });
+    res.json(tasks);
+  } catch (error) {
+    console.error('Error fetching tasks:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
+
+async function deleteTask(req, res) {
+  try {
+    const taskId = req.params.id;
+
+    if (!taskId) {
+      return res.status(400).json({ message: "Task ID is required for deletion." });
+    }
+
+    const result = await Task.findByIdAndDelete(taskId);
+
+    if (!result) {
+      return res.status(404).json({ message: "Task not found." });
+    }
+
+    res.json({ message: "Task deleted successfully." });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+async function updateTask(req, res) {
+  try {
+    const taskId = req.params.id; 
+    const task = req.body;
+
+    const result = await Task.findByIdAndUpdate(taskId, task, { new: true });
+
+    if (result) {
+      res.json(result);
+    } else {
+      res.status(404).json({ message: 'Task not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+}
+
 module.exports = {
   createTask,
+  getTasks,
+  deleteTask,
+  updateTask,
 };
